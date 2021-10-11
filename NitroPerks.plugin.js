3869a845d6affe97d360f864a2c66b55ea151e05
cloud <62519659+lem6ns@users.noreply.github.com>
2021-10-11 10:13:20 -0600

    yeah man

--- NitroPerks.plugin.js
+++ NitroPerks.plugin.js
@@ -1,29 +1,150 @@
 /**
  * @name NitroPerks
- * @author lemons
- * @description Abandoned.
- * @version 2.0.0
- * @invite inviteCode
- * @authorId 407348579376693260
+ * @website https://github.com/respecting/NitroPerks
  * @source https://raw.githubusercontent.com/respecting/NitroPerks/main/NitroPerks.plugin.js
  * @updateUrl https://raw.githubusercontent.com/respecting/NitroPerks/main/NitroPerks.plugin.js
  */
-
- module.exports = class NitroPerks {
-    load() {BdApi.showConfirmationModal("Notice:", 
-    [
-        "NitroPerks has been abandoned.",
-        "Alternatives you can use are:",
-        BdApi.React.createElement("a", {href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank"}, "FreeEmojis by An00nymushun"),"",
-BdApi.React.createElement("a", {href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank"}, "EditUsers by DevilBro")
-    ],
-    {
-        danger: true,
-        confirmText: "Okay.",
-        cancelText: ""
+/*@cc_on
+@if (@_jscript)
+	
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
     }
-);}
+    WScript.Quit();
 
-    start() {} 
-    stop() {}
-}
+@else@*/
+module.exports = (() => {
+    const config = {
+        "info": {
+            "name": "NitroPerks",
+            "authors": [{
+                "name": "lemons",
+                "discord_id": "407348579376693260",
+                "github_username": "respecting"
+            }],
+            "version": "2.0.0",
+            "description": "Abandoned!",
+            "github": "https://github.com/respecting/NitroPerks",
+            "github_raw": "https://raw.githubusercontent.com/respecting/NitroPerks/main/NitroPerks.plugin.js"
+        },
+        "main": "NitroPerks.plugin.js"
+    };
+
+    return !global.ZeresPluginLibrary ? class {
+        constructor() {
+            this._config = config;
+        }
+        getName() {
+            return config.info.name;
+        }
+        getAuthor() {
+            return config.info.authors.map(a => a.name).join(", ");
+        }
+        getDescription() {
+            return config.info.description;
+        }
+        getVersion() {
+            return config.info.version;
+        }
+        load() {
+            BdApi.showConfirmationModal("Notice:",
+                [
+                    "NitroPerks has been abandoned.",
+                    "Alternatives you can use are:",
+                    BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
+                    BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
+                ],
+                {
+                    danger: true,
+                    confirmText: "Okay.",
+                    cancelText: ""
+                }
+            );
+        }
+        start() { }
+        stop() { }
+    } : (([Plugin, Api]) => {
+        const plugin = (Plugin, Api) => {
+            return class NitroPerks extends Plugin {
+                getSettingsPanel() {
+                    return BdApi.showConfirmationModal("Notice:",
+                        [
+                            "NitroPerks has been abandoned.",
+                            "Alternatives you can use are:",
+                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
+                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
+                        ],
+                        {
+                            danger: true,
+                            confirmText: "Okay.",
+                            cancelText: ""
+                        }
+                    );
+                }
+
+                saveAndUpdate() {
+                    BdApi.showConfirmationModal("Notice:",
+                        [
+                            "NitroPerks has been abandoned.",
+                            "Alternatives you can use are:",
+                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
+                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
+                        ],
+                        {
+                            danger: true,
+                            confirmText: "Okay.",
+                            cancelText: ""
+                        }
+                    );
+                }
+                onStart() {
+                    BdApi.showConfirmationModal("Notice:",
+                        [
+                            "NitroPerks has been abandoned.",
+                            "Alternatives you can use are:",
+                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
+                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
+                        ],
+                        {
+                            danger: true,
+                            confirmText: "Okay.",
+                            cancelText: ""
+                        }
+                    );
+                }
+
+                onStop() {
+                    BdApi.showConfirmationModal("Notice:",
+                        [
+                            "NitroPerks has been abandoned.",
+                            "Alternatives you can use are:",
+                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/FreeEmojis", target: "_blank" }, "FreeEmojis by An00nymushun"), "",
+                            BdApi.React.createElement("a", { href: "https://betterdiscord.app/plugin/EditUsers", target: "_blank" }, "EditUsers by DevilBro")
+                        ],
+                        {
+                            danger: true,
+                            confirmText: "Okay.",
+                            cancelText: ""
+                        }
+                    );
+                }
+            };
+        };
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+/*@end@*/
